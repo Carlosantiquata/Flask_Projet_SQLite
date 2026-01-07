af92f94d684db745e5aea907644954514991ee0e
Carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2026-01-07 15:56:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -76,10 +76,10 @@ def enregistrer_client():
     conn.commit()
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
+                                                                                                                                       
+if __name__ == "__main__":
+  app.run(debug=True)
 
 @app.route('/fiche_nom/')
 def fiche_nom():
     return "<h1>Recherche de client</h1><p>Bienvenue sur l'interface de recherche.<p>"
-                                                                                                                                       
-if __name__ == "__main__":
-  app.run(debug=True)
