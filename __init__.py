d575590d1e1ad0285a727e2193957f0d51336b13
Carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2026-01-28 16:49:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ def est_authentifie():
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('biblio.html')
 
 @app.route('/lecture')
 def lecture():
